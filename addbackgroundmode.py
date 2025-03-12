0e9d13c640f9aeb79bfd2667cdc18c44aca6e9dd
Sulav Kafle <sulavkafle7@gmail.com>
2025-03-12 06:16:19 +0545

    included exception for some cmds

--- addbackgroundmode.py
+++ addbackgroundmode.py
@@ -2,11 +2,15 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+excludecmd= os.getenv("EXCLUDE_CMD").split(";")
+# print(excludecmd)
+
 with open(os.getenv("ALIAS_LOCATION"), "r") as file:
     a= file.readlines()
 # print(a)
 for i in a:
-    if i.__contains__("doskey"):
+    if i.__contains__("doskey") and not i.__contains__("/b") and i[i.index(" ")+1:i.index("=")] not in excludecmd:
+        # print(i)
         index= a.index(i)
         frontpart= i[:i.index("=")+1]
         if i.__contains__("\n"):
