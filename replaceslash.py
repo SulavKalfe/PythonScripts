c4dc80ace146aa7161825e8d7219bc4f0fcee73d
Sulav Kafle <sulavkafle7@gmail.com>
2025-03-12 06:17:49 +0545

    made a change

--- replaceslash.py
+++ replaceslash.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
     load_dotenv()
     name= input("Provide variable name to add to the .env file:")
     with open(os.getenv("ENV_LOCATION"), "a")as file:
-        file.write(f'\n{name.upper().replace(" ", "_")}="{pyperclip.paste().replace("\\", "/")}"')
+        file.write(f'{name.upper().replace(" ", "_")}="{pyperclip.paste().replace("\\", "/")}"\n')
